7cfa26cc91ab3db9b3f7c3f4d546900e0a761b30
Harish A <78593477+harish722001@users.noreply.github.com>
2021-03-15 12:00:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const server = http.createServer(app);
 
 server.listen(port, () => {
-    console.log("Server runninong on port 3000...");
+    console.log('Server running on port :8080...');
 });
+
