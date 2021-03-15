0d42f317cb0af8215535d0523ca9e060adc77567
Harish A <78593477+harish722001@users.noreply.github.com>
2021-03-15 12:01:53 +0530

    Update register.js

--- routes/register.js
+++ routes/register.js
@@ -1,30 +1,57 @@
 const express = require('express');
 const mysql = require('mysql');
+const bcrypt = require('bcrypt');
 const router = express.Router();
+
+const saltRounds = 10;
 
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
-    database : 'nodetest'
+    password: 'drowssap123',
+    database : 'foodapp'
 })
 
 
-router.post('/', (req, res, _next) =>{
-    const post = {
-        username : req.body.username,
-        password : req.body.password,
-        email : req.body.email
-    }
+router.post('/', async function (req, res, _next) {
 
-        let sql = 'INSERT INTO user SET ?'
-        let query = db.query(sql, post, err => {
-        if(err){
-            throw err
-        }
-        res.send('Employee added')
-        })
+        const password = req.body.password;
+        const encryptedpassword = await bcrypt.hash(password, saltRounds);
 
 
-});
+        const user = {
+            nameFirst: req.body.firstname,
+	        nameLast: req.body.lastname,	
+            email: req.body.email,
+	        contact : req.body.contact,
+	        contactAdditional : req.body.additionalcontact, 
+	        password: encryptedpassword
+	    
+        };
+
+        db.query("SELECT * FROM users WHERE email = ?", [req.body.email], function (err, result, _fields) {
+            db.on(err, function (err) {
+                res.json({ message:"error"});
+            });
+
+            if (result && result.length) {
+                res.json({ message:"email already exists"});
+            }
+
+            else {
+                let sql = 'INSERT INTO users SET ?';
+                let query = db.query(sql, user, err => {
+                    if (err) {
+                        res.json({message:"something went wrong!"});
+                    }
+                    res.json({ message:"employee added"});
+                });
+            }
+        });
+
+
+
+
+    });
+
 module.exports = router;
